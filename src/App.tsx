46b6efbe5058188be9f82049f4b5c184badbabb1
AnnAlh <anya.alh@gmail.com>
2023-11-06 02:58:16 +0300

    1-9

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./components/Technologies";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+        <Header />
+        <Technologies />
     </div>
   );
 }
+
+
+
 
 export default App;
